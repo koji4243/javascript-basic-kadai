1fd489b00336896e5c5ae298656bc88a772badb5
さくらの助 <koji.19@icloud.com>
2025-08-20 10:01:56 +0900

    kadai_005プッシュしました

---
+++ kadai_005/bmi.js
@@ -0,0 +1,15 @@
+
+
+
+let height;
+let weight;
+
+function bmiCalc(height, weight) {
+    let heightMeter = height / 100;
+    let bmi = weight / (heightMeter * heightMeter);
+    console.log(bmi);
+    return bmi;
+}
+
+//　　身長cm　体重kg表記
+bmiCalc(172, 78);
