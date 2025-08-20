4e4a82ae0045ea565116709d57f47d775ebd1712
さくらの助 <koji.19@icloud.com>
2025-08-20 13:22:44 +0900

    toda変数一部修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,4 +1,4 @@
 
 let today = new Date();
 
-console.log(today.getFullYear() + "年" + today.getMonth() + "月" + today.getDay() + "日");
+console.log(today.getFullYear() + "年" + (today.getMonth() + 1) + "月" + today.getDate() + "日");
