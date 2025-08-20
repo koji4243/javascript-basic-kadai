2d995acd52be5b2d445183a90c07bd4c2851a2e3
さくらの助 <koji.19@icloud.com>
2025-08-20 11:24:28 +0900

    kadai_007プッシュします。

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+
+let num = 2056;
+
+switch(true) { 
+    case( num % 15 === 0):
+        console.log('3と5の倍数です'); 
+        break;
+    case(num % 5 === 0):
+        console.log('5の倍数です');
+        break;
+    case (num % 3 === 0):
+        console.log('3の倍数です');
+        break;
+    default:
+        console.log(num);
+} 
