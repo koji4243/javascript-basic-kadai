e9f6fdd6b47454dfc83600b9d80a9b0cd51e6682
さくらの助 <koji.19@icloud.com>
2025-08-20 10:50:44 +0900

    変数へ値代入

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,15 +1,15 @@
 
 
-let bmi;
-let height;
-let weight;
+let bmi = 55;
+let height = 172;
+let weight = 72;
 
-function bmiCalc(height, weight) {
+function bmiCalc() {
     let heightMeter = height / 100;
     let bmi = weight / (heightMeter * heightMeter);
     console.log(bmi);
     return bmi;
 }
 
-//　　身長cm　体重kg表記
-bmiCalc(172, 78);
+
+bmiCalc();
