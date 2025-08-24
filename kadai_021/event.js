3da30957e9ff1bcdb71686e6b891eb1b0b339c6d
さくらの助 <koji.19@icloud.com>
2025-08-24 09:58:55 +0900

    kadai_021プッシュしました

---
+++ kadai_021/event.js
@@ -0,0 +1,8 @@
+const h2 = document.getElementById("text");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    setTimeout(() => {
+        h2.textContent = "ボタンをクリックしました";
+    }, 2000);
+});
